6ca71a73d311337d8919429d3acb0b8f341444b4
Emmett <76142235+snakesford@users.noreply.github.com>
2023-07-11 13:16:05 -0700

    stuff

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <h1>Hello World!</h1>
         <img src={logo} className="App-logo" alt="logo" />
+        {/*stuff*/}
       </header>
     </div>
   );
